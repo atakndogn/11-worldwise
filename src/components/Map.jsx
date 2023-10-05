f9418c4bc3ce93f5ec46ec9c9f03d8e0615233bb
Atakan <atakan.dogan@gmail.com>
2023-10-05 14:56:56 +0300

    212 - Nested Routes and Index Route

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,18 @@
 import styles from "./Map.module.css";
+import { NavLink } from "react-router-dom";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  return (
+    <div className={styles.mapContainer}>
+      {" "}
+      <h1>Map</h1>
+      <ul>
+        <li>
+          <NavLink to="form">FORM</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Map;
