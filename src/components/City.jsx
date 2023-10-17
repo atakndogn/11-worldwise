a5225414966ab916ff591cbf9d3f1ba052279948
Atakan <atakan.dogan@gmail.com>
2023-10-17 15:39:24 +0300

    216 - Dynamic Routes With URL Parameters

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -9,7 +10,8 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  // TEMP DATA
+  const { id } = useParams();
+
   const currentCity = {
     cityName: "Lisbon",
     emoji: "🇵🇹",
@@ -19,6 +21,12 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
+  return (
+    <h1>
+      {cityName} - {id}
+    </h1>
+  );
+  /*
   return (
     <div className={styles.city}>
       <div className={styles.row}>
@@ -51,11 +59,10 @@ function City() {
         </a>
       </div>
 
-      <div>
-        <ButtonBack />
-      </div>
+      <div></div>
     </div>
   );
+  */
 }
 
 export default City;
